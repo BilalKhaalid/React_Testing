f2cabcfb831b3562f7f7111e2e6c82a46ee37191
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-03 09:36:33 +0500

    Grouping tests together with describe method

--- cra/src/components/Greet/Greet.test.jsx
+++ cra/src/components/Greet/Greet.test.jsx
@@ -13,14 +13,42 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("greet render correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+// test("greet render correctly", () => {
+//   render(<Greet />);
+//   const textElement = screen.getByText("Hello");
+//   expect(textElement).toBeInTheDocument();
+// });
+
+// test("greet render with a name", () => {
+//   render(<Greet name="Bilal" />);
+//   const textElement = screen.getByText("Hello Bilal");
+//   expect(textElement).toBeInTheDocument();
+// });
+
+// ! We can group test together using following syntax
+
+describe("Greet tests", () => {
+  test("greet render correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+  // // ! We can have nested group inside groups
+  // describe("nested", () => {
+  //   test("greet render with a name", () => {
+  //     render(<Greet name="Bilal" />);
+  //     const textElement = screen.getByText("Hello Bilal");
+  //     expect(textElement).toBeInTheDocument();
+  //   });
+  // });
 });
 
-test("greet render with a name", () => {
-  render(<Greet name="Bilal" />);
-  const textElement = screen.getByText("Hello Bilal");
-  expect(textElement).toBeInTheDocument();
+// ! We can have as many groups of test as we want
+
+describe("nested", () => {
+  test("greet render with a name", () => {
+    render(<Greet name="Bilal" />);
+    const textElement = screen.getByText("Hello Bilal");
+    expect(textElement).toBeInTheDocument();
+  });
 });

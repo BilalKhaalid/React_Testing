e448d60937d04d5cf05b15d5df052590d0cda6f6
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-08 15:34:47 +0500

    mocking http requests setup

---
+++ cra/src/components/users/users.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const users = () => {
+  const [users, setUsers] = React.useState([]);
+  const [error, setError] = useState(null);
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.map((user) => user.name)))
+      .catch(() => setError("Error fetching users"));
+  }, []);
+  return (
+    <div>
+      <h1>Users</h1>
+      {error && <p>{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default users;
